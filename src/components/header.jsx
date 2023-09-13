accf6fb08549bce57a06c0430bee4a604567b405
EilonV <73593619+EilonV@users.noreply.github.com>
2023-09-13 18:54:32 +0300

    header after login cookies

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,27 +1,47 @@
-import { NavLink } from "react-router-dom"
+import { NavLink, useNavigate } from "react-router-dom"
 
 export const Header = () => {
+    let cookies
+    if (document.cookie) cookies = document.cookie.split(";");
+    const navigate = useNavigate()
     const removeCookies = () => {
-        const cookies = document.cookie.split(";");
         for (var i = 0; i < cookies.length; i++) {
-          console.log(cookies[0]);
+            var cookie = cookies[i];
+            var eqPos = cookie.indexOf("=");
+            var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+            document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/";
         }
+        navigate('/login')
     }
+    if (cookies) var name = cookies[1].split('=')[1]
     return <header className="main-layout">
         <nav className="flex align-center space-between">
             <span className="logo">ZYADB</span>
-            <div className="flex align-center header-links">
+            <div className="header-links flex align-center ">
+
                 <NavLink to="/" className={({ isActive, isPending }) =>
                     isPending ? "pending" : isActive ? "active" : ""
                 }>חגיגות</NavLink>
-                <NavLink to="/login" className={({ isActive, isPending }) =>
-                    isPending ? "pending" : isActive ? "active" : ""
-                }>התחברות</NavLink>
-                <NavLink to="/register" className={({ isActive, isPending }) =>
-                    isPending ? "pending" : isActive ? "active" : ""
-                }>הרשמה</NavLink>
+
+                {!cookies &&
+                    <NavLink to="/login" className={({ isActive, isPending }) =>
+                        isPending ? "pending" : isActive ? "active" : ""
+                    }>התחברות</NavLink>
+                }
+
+                {!cookies &&
+                    <NavLink to="/register" className={({ isActive, isPending }) =>
+                        isPending ? "pending" : isActive ? "active" : ""
+                    }>הרשמה</NavLink>
+                }
+
+                {cookies && <div className="logged-header flex align-center">
+                    <p className="logout" onClick={removeCookies}>התנתקות</p>
+                    <p className="logged-name"><span>{name}</span> ,היי</p>
+                </div>}
+
             </div>
         </nav>
-        <button onClick={removeCookies}>התנתקות</button>
+        {/* <button onClick={removeCookies}>התנתקות</button> */}
     </header>
 }
