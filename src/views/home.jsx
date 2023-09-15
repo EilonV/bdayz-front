ebc6e807a035f44a8e8f266fd4be12643c2ba349
EilonV <73593619+EilonV@users.noreply.github.com>
2023-09-16 00:32:10 +0300

    add bday popup

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -2,11 +2,12 @@ import { useDispatch } from "react-redux";
 import { httpsService } from "../services/https.service"
 import { UseSelector, useSelector } from "react-redux/es/hooks/useSelector"
 import { getBdayz, getUser } from "../store/user/userActions";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 export const Home = () => {
     const bdayz = useSelector(state => state.bdayz)
     const dispatch = useDispatch()
+    const addBdayRef = useRef()
 
     useEffect(() => {
         document.cookie &&
@@ -33,7 +34,7 @@ export const Home = () => {
     // }
 
     const displayDate = (date) => {
-        return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
+        return `${new Date(date).getDay()}/${new Date(date).getMonth() + 1}/${new Date(date).getFullYear()}`
     }
 
     const addBday = () => {
@@ -43,17 +44,43 @@ export const Home = () => {
         }
         httpsService.addBday('64fc85085e90bb58b91cf234', data)
     }
+    const handleBdayForm = (e) => {
+        e.preventDefault()
+        const name = e.target[0].value
+        const date = e.target[2].value
+        const bday = {
+            name: e.target[0].value,
+            date: e.target[2].value
+        }
+        console.log('NAME DATE', name, date);
+    }
+    const openAddBdayModal = () => {
+        addBdayRef.current.classList.add('active')
+    }
+    const closeAddBdayModal = () => {
+        addBdayRef.current.classList.remove('active')
+    }
 
     return <section className="home main-layout">
         <div className="bdayz">
             {bdayz.map((bday) => {
                 return <div className="bday" key={bday.name}>
                     <h1>{bday.name}</h1>
-                    <p>{bday.date}</p>
-                    {/* {displayDate(bday.date)} */}
+                    <p>{displayDate(bday.date)}</p>
                 </div>
             })}
         </div>
-        <button>הוסף</button>
+        <button onClick={openAddBdayModal}>הוסף</button>
+        <div className="add-bday-modal full" ref={addBdayRef}>
+            <div className="bg" onClick={closeAddBdayModal}></div>
+            <form className="add-bday-form flex column align-center" onSubmit={handleBdayForm}>
+                <button type="button" className="close-modal" onClick={closeAddBdayModal}>X</button>
+                <label htmlFor="name">שם החוגג</label>
+                <input type="text" name="name" id="name" />
+                <label htmlFor="date">תאריך החוגג</label>
+                <input type="date" name="date" id="date" min="1900-01-01" max={`${new Date().getFullYear()}-12-31`} />
+                <button >הוספה</button>
+            </form>
+        </div>
     </section>
 }
