45bf0d27c937de3e784e86b4d3e5facc942f6d8a
EilonV <73593619+EilonV@users.noreply.github.com>
2023-09-27 17:32:31 +0300

    baseUrl

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: "http://localhost:5000/api/v1/users",
+    baseURL: "https://bdayz-back.onrender.com/api/v1/users",
     headers: {
         "Content-type": "application/json"
     }
 })
+
+// baseURL: "http://localhost:5000/api/v1/users",
