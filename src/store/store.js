bd6463d2e536c21450998ac6f1938cd65c9f5eea
EilonV <73593619+EilonV@users.noreply.github.com>
2023-10-02 17:01:08 +0300

    removed logger middleware

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,6 @@
 import { createStore,applyMiddleware } from "redux";
 import userReducuer from "./user/userReducer";
-import logger from "redux-logger";
-const store = createStore(userReducuer,applyMiddleware(logger))
+// import logger from "redux-logger";
+const store = createStore(userReducuer,applyMiddleware())
 
 export default store
